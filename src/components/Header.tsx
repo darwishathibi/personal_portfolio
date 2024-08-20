63a2170be009796f3d4d4e4c05f769a6a35b530e
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-20 19:56:00 +0800

    navbar background blur

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <nav>
+    <nav className="fixed top-0 backdrop-blur-sm w-full">
       <div
-        className="flex justify-center items-center h-16 bg-transparent relative md:bg-fixed md:w-screen text-lg"
+        className="flex justify-center items-center h-16 bg-transparent text-lg"
         role="navigation"
       >
         <a href="/about" className="px-20">
