aa9f05b995b6d8e2f419354565bc30dcfa810349
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-22 02:12:29 +0800

    revert back, just only use sentences

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -1,9 +1,13 @@
+"use client";
+import { text } from "stream/consumers";
+import { TextGenerateEffect } from "../components/ui/text-generate-effect";
 
-
-const sentences =
-  "a network security student from IIUM, Malaysia with a passion for photography and tech. I enjoy developing software (but never release it, lol) and tackling CTF challenges, constantly honing my skills and expanding my knowledge in the digital world. I thrive on solving complex problems and always eager to learn and grow.";
+const sentences = 
+  "a network security student from IIUM, Malaysia with a passion for photography and tech. I enjoy developing software (but never release it, lol) and tackling CTF challenges, constantly upgrading my skills and expanding my knowledge in the digital world. I thrive on solving complex problems and always eager to learn and grow.";
 
 const highlightColor = "#5C8374";
+
+// const test ="hello <>s;
 
 const highlightWords = [
   "IIUM",
@@ -16,36 +20,14 @@ const highlightWords = [
   "eager to learn",
 ];
 
-// Function to dynamically apply highlights
-function highlightText(
-  text: string,
-  wordsToHighlight: string[],
-  highlightColor: string
-) {
-  // Split the text into words while preserving punctuation
-  const regex = new RegExp(`(${wordsToHighlight.join("|")})`, "gi");
-
-  // Split the text by the highlighted words and map over them
-  return text.split(regex).map((part, index) => {
-    if (wordsToHighlight.includes(part)) {
-      return (
-        <b key={index} style={{ color: highlightColor }}>
-          {part}
-        </b>
-      );
-    }
-    return part;
-  });
-}
-
 export default function About() {
   return (
     <div className="flex flex-col items-center justify-center h-screen relative">
-      <div className="absolute text-white/10 text-[450px] overflow-hidden -top-36 right-0 font-extrabold tracking-wider">
+      <div className="absolute text-white/10 text-[150px] sm:text-[250px] md:sm:text-[350px] lg:sm:text-[450px] overflow-hidden sm:-top-28 right-0 lg:-top-36 right-0 font-extrabold tracking-wide">
         <p>About</p>
       </div>
-      <div className="text-xl sm:text-2xl md:text-3xl lg:text-4xl space-y-4 w-5/12 tracking-wider">
-        {highlightText(sentences, highlightWords, highlightColor)}
+      <div className="text-md sm:text-2xl md:text-3xl lg:text-4xl space-y-4 w-5/12 tracking-wider text-white mb-10">
+        <TextGenerateEffect words={sentences} />
       </div>
     </div>
   );
