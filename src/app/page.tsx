e5a39278c15b95f3cf001f80cc9017568dd45db2
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-21 17:57:26 +0800

    throw header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Intro from "./Intro";
 import About from "./About";
+import Projects from "./Projects";
 
 export default function Home() {
   return (
     <main className="font-sf tracking-widest flex min-h-screen flex-col items-center justify-between bg-zinc-900 bg-[radial-gradient(#27272a_1px,transparent_1px)] [background-size:16px_16px] scrollbar-hide">
-      <Header />
+      {/* <Header /> */}
       <Intro />
       <About />
+      <Projects />
       <Footer />
     </main>
   );
