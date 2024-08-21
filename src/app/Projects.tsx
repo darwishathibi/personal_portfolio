c9da66befb70baabb171b76b40d7d14846a628d2
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-22 02:13:22 +0800

    add cards

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -1,11 +1,85 @@
-export default function Projects() {
+import React from "react";
+import { BentoGrid, BentoGridItem } from "../components/ui/bento-grid";
+import {
+  IconArrowWaveRightUp,
+  IconBoxAlignRightFilled,
+  IconBoxAlignTopLeft,
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
+
+export default function BentoGridDemo() {
   return (
-    <div className="h-screen border-solid border-2 border-sky-500">
-      <img
-        src="https://photos.app.goo.gl/avH11w4ZZUXQrx3K8"
-        alt="The head and torso of a dinosaur skeleton;
-          it has a large head with long sharp teeth" 
-      />
-    </div>
+    <>
+      <div className="border-solid h-full w-full my-96 relative">
+        <div className="text-xl sm:text-2xl md:texl-3xl lg:text-4xl flex justify-center items-center">
+          <p className="mb-32">
+            things i <span className="text-green-idk">made</span>...
+          </p>
+        </div>
+        <BentoGrid className="max-w-4xl mx-auto">
+          {items.map((item, i) => (
+            <BentoGridItem
+              key={i}
+              title={item.title}
+              description={item.description}
+              header={item.header}
+              icon={item.icon}
+              className={i === 1 || i === 2 ? "md:col-span-2" : ""}
+            />
+          ))}
+        </BentoGrid>
+      </div>
+    </>
   );
 }
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 "></div>
+);
+const items = [
+  {
+    title: "The Dawn of Innovation",
+    description: "Explore the birth of groundbreaking ideas and inventions.",
+    header: <Skeleton />,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Digital Revolution",
+    description: "Dive into the transformative power of technology.",
+    header: <Skeleton />,
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Art of Design",
+    description: "Discover the beauty of thoughtful and functional design.",
+    header: <Skeleton />,
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Power of Communication",
+    description:
+      "Understand the impact of effective communication in our lives.",
+    header: <Skeleton />,
+    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  },
+  // {
+  //   title: "The Pursuit of Knowledge",
+  //   description: "Join the quest for understanding and enlightenment.",
+  //   header: <Skeleton />,
+  //   icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+  // },
+  // {
+  //   title: "The Joy of Creation",
+  //   description: "Experience the thrill of bringing ideas to life.",
+  //   header: <Skeleton />,
+  //   icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+  // },
+  // {
+  //   title: "The Spirit of Adventure",
+  //   description: "Embark on exciting journeys and thrilling discoveries.",
+  //   header: <Skeleton />,
+  //   icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+  // },
+];
