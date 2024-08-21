67bc0da35cc84cfec92385e3951a0e19da4248ca
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-21 17:57:39 +0800

    add projects page

---
+++ src/app/Projects.tsx
@@ -0,0 +1,11 @@
+export default function Projects() {
+  return (
+    <div className="h-screen border-solid border-2 border-sky-500">
+      <img
+        src="https://photos.app.goo.gl/avH11w4ZZUXQrx3K8"
+        alt="The head and torso of a dinosaur skeleton;
+          it has a large head with long sharp teeth" 
+      />
+    </div>
+  );
+}
