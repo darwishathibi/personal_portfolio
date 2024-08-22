fdbd859ea5542bf8e046d7091ba6e753bffd3a89
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-22 19:31:48 +0800

    change card display

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import projectsData from "../data/projects.json";
 import { BentoGrid, BentoGridItem } from "../components/ui/bento-grid";
 import {
   IconArrowWaveRightUp,
   IconBoxAlignRightFilled,
   IconBoxAlignTopLeft,
-  IconClipboardCopy,
+  IconClipboardCopy,  
   IconFileBroken,
   IconSignature,
   IconTableColumn,
@@ -13,21 +14,23 @@ import {
 export default function BentoGridDemo() {
   return (
     <>
-      <div className="border-solid h-full w-full my-96 relative">
+      <div className="border-solid h-full w-full my-96 relative h-full">
         <div className="text-xl sm:text-2xl md:texl-3xl lg:text-4xl flex justify-center items-center">
           <p className="mb-32">
             things i <span className="text-green-idk">made</span>...
           </p>
         </div>
-        <BentoGrid className="max-w-4xl mx-auto">
-          {items.map((item, i) => (
+        <BentoGrid className="mx-auto order-solid">
+          {projectsData.map((item, i) => (
             <BentoGridItem
               key={i}
               title={item.title}
               description={item.description}
-              header={item.header}
-              icon={item.icon}
-              className={i === 1 || i === 2 ? "md:col-span-2" : ""}
+              className=""
+              imageURI={item.imageURI}
+              frameworks={item.frameworks}
+              github={item.github}
+              demo={item.demo}
             />
           ))}
         </BentoGrid>
@@ -35,9 +38,14 @@ export default function BentoGridDemo() {
     </>
   );
 }
+
+
+//test data
 const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 "></div>
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-white"></div>
 );
+
+
 const items = [
   {
     title: "The Dawn of Innovation",
