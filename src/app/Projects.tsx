8de0cb34589a99acc32f55ef78ca88fb985c1d45
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-26 02:08:11 +0800

    change colour

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -17,7 +17,7 @@ export default function BentoGridDemo() {
       <div className="border-solid h-full w-full my-96 relative h-full">
         <div className="text-xl sm:text-2xl md:texl-3xl lg:text-4xl flex justify-center items-center">
           <p className="mb-32">
-            things i <span className="text-green-idk">made</span>...
+            things i <span className="text-indigo-500">made</span>...
           </p>
         </div>
         <BentoGrid className="mx-auto order-solid">
@@ -38,56 +38,3 @@ export default function BentoGridDemo() {
     </>
   );
 }
-
-
-//test data
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-white"></div>
-);
-
-
-const items = [
-  {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-  },
-  // {
-  //   title: "The Pursuit of Knowledge",
-  //   description: "Join the quest for understanding and enlightenment.",
-  //   header: <Skeleton />,
-  //   icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
-  // },
-  // {
-  //   title: "The Joy of Creation",
-  //   description: "Experience the thrill of bringing ideas to life.",
-  //   header: <Skeleton />,
-  //   icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
-  // },
-  // {
-  //   title: "The Spirit of Adventure",
-  //   description: "Embark on exciting journeys and thrilling discoveries.",
-  //   header: <Skeleton />,
-  //   icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-  // },
-];
