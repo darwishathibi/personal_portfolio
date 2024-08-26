db265c1f9bf6a34d115d23b987e056e7499496b2
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-27 02:01:08 +0800

    add return component

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -1,12 +1,14 @@
 import Intro from "@/app/photography/Intro"
 import Footer from "@/components/Footer";
 import Gear from "@/app/photography/Gear"; 
+import Return from "@/app/photography/Return";
 
 export default function PhotographyHome() {
     return (
       <main className="font-sf tracking-widest flex min-h-screen flex-col items-center justify-between bg-slate-950 scrollbar-hide">
         <Intro />
         <Gear />
+        <Return />
         <Footer />
       </main>
     );
