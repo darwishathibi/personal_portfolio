92d723c7b378731ddd7a166dfc00aa326a569f4e
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-29 07:38:28 +0800

    add test data

--- src/app/photography/[gearID]/ImagesCard.tsx
+++ src/app/photography/[gearID]/ImagesCard.tsx
@@ -1,6 +1,15 @@
 import { FocusCards } from "@/components/ui/focus-card";
+import imagesData from "@/data/images.json";
 
-export default function ImagesCard() {
+type GearID = keyof typeof imagesData.images;
+
+export default function ImagesCard({ params }: { params: { gearID: GearID } }) {
+  const { gearID } = params;
+  // console.log(gearID);
+
+  // Access the property using gearID
+  const gearImages = imagesData.images[gearID];
+  // console.log(gearImages);
   const cards = [
     {
       title: "Forest Adventure",
