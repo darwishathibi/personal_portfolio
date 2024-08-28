4092ad2e976cc4901bd332bb76e8f556f8949e28
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-29 07:38:49 +0800

    add footer and params

--- src/app/photography/[gearID]/page.tsx
+++ src/app/photography/[gearID]/page.tsx
@@ -1,9 +1,11 @@
 import ImagesCard from "@/app/photography/[gearID]/ImagesCard";
+import Footer from "@/components/Footer";
 
 export default function GearId({params} : any){
     return (
-        <main className="bg-slate-950 scrollbar-hide">
-            <ImagesCard />
+        <main className="bg-slate-950 scrollbar-hide tracking-widest">
+            <ImagesCard params={params}/>
+            <Footer />
         </main>
     )
 }   
