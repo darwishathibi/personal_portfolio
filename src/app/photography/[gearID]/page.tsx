16bfce55e2a09a114e436061ee08e4158ea2f1cc
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-29 23:20:50 +0800

    add return button and reconstruct the screen height

--- src/app/photography/[gearID]/page.tsx
+++ src/app/photography/[gearID]/page.tsx
@@ -1,11 +1,15 @@
 import ImagesCard from "@/app/photography/[gearID]/ImagesCard";
 import Footer from "@/components/Footer";
+import Return from "@/app/photography/[gearID]/Return";
 
 export default function GearId({params} : any){
     return (
-        <main className="bg-slate-950 scrollbar-hide tracking-widest">
-            <ImagesCard params={params}/>
-            <Footer />
-        </main>
-    )
+      <main className="bg-slate-950 scrollbar-hide tracking-widest">
+        <div className="h-full">
+          <ImagesCard params={params} />
+        </div>
+        <Return />
+        <Footer />
+      </main>
+    );
 }   
