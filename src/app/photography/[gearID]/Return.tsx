5e1a79bd1966ab9ce268fa67bc083c5c09ad26cb
darwishathibi <danish.darwis@live.iium.edu.my>
2024-08-29 23:20:59 +0800

    add return button

---
+++ src/app/photography/[gearID]/Return.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Link from "next/link";
+import DrawOutlineButton from "@/components/ui/draw-outline-button";
+
+export default function Return() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <Link href={"/photography"}>
+        <DrawOutlineButton>return back.</DrawOutlineButton>
+      </Link>
+    </div>
+  );
+}
