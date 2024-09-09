8f96a2982dcb2f20a75de3e37eef6ebc50e2855e
darwishathibi <danish.darwis@live.iium.edu.my>
2024-09-09 22:34:23 +0800

    change bg a lil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Contact from "./Contact";
 
 export default function Home() {
   return (
-    <main className="font-sf tracking-widest flex min-h-screen flex-col items-center justify-between bg-zinc-900 bg-[radial-gradient(#27272a_1px,transparent_1px)] [background-size:16px_16px] scrollbar-hide">
+    <main className="font-sf tracking-widest flex min-h-screen flex-col items-center justify-between bg-zinc-900 bg-[radial-gradient(#27272a_1px,transparent_2px)] [background-size:16px_16px] scrollbar-hide">
       {/* <Header /> */}
       <Intro />
       <About />
