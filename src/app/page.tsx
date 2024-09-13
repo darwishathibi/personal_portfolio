736d27947ef59555e8d1c49ecfe537cc7a687e32
darwishathibi <danish.darwis@live.iium.edu.my>
2024-09-14 05:39:05 +0800

    add tracing beam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,21 @@ import Photography from "./Photography";
 import Skills from "./Skills";
 import Contact from "./Contact";
 
+import { TracingBeam } from "@/components/ui/tracing-beam";
+
 export default function Home() {
   return (
-    <main className="text-white font-sf tracking-widest flex min-h-screen flex-col items-center justify-between bg-zinc-900 bg-[radial-gradient(#27272a_1px,transparent_2px)] [background-size:16px_16px] overflow-hidden">
-      {/* <Header /> */}
-      <Intro />
-      <About />
-      <Projects />
-      <Photography />
-      <Skills />
-      <Contact />
-      <Footer />
-    </main>
+    <TracingBeam className="h-full bg-zinc-900 bg-[radial-gradient(#27272a_1px,transparent_2px)] [background-size:16px_16px] overflow-hidden">
+      <main className="text-white font-sf tracking-widest flex min-h-screen flex-col items-center justify-between">
+        {/* <Header /> */}
+        <Intro />
+        <About />
+        <Projects />
+        <Photography />
+        <Skills />
+        <Contact />
+        <Footer />
+      </main>
+    </TracingBeam>
   );
 }
