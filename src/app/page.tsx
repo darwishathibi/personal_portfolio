bd8513579bbf61e12469cce7f2018d9a768c4844
darwishathibi <danish.darwis@live.iium.edu.my>
2024-09-14 02:09:04 +0800

    make the overflow is hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Contact from "./Contact";
 
 export default function Home() {
   return (
-    <main className="text-white font-sf tracking-widest flex min-h-screen flex-col items-center justify-between bg-zinc-900 bg-[radial-gradient(#27272a_1px,transparent_2px)] [background-size:16px_16px] scrollbar-hide w-full">
+    <main className="text-white font-sf tracking-widest flex min-h-screen flex-col items-center justify-between bg-zinc-900 bg-[radial-gradient(#27272a_1px,transparent_2px)] [background-size:16px_16px] overflow-hidden">
       {/* <Header /> */}
       <Intro />
       <About />
